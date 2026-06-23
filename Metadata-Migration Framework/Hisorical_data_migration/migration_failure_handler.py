51b1932a2874f52295fb9388c79ff2798a01e977
Charless Binny <charlesweslyinchrist@gmail.com>
2026-06-23 10:17:17 +0530

    Create migration_failure_handler.py

---
+++ Metadata-Migration Framework/Hisorical_data_migration/migration_failure_handler.py
@@ -0,0 +1,122 @@
+# ==========================================
+# migration_failure_handler.py
+# ==========================================
+
+from pyspark.sql import Row
+from datetime import datetime
+
+%run ../Common/config
+%run ../Common/logger
+%run ../Common/storage_manager
+
+
+class MigrationFailureHandler:
+
+    def __init__(self):
+
+        self.logger = Logger()
+
+        self.storage = StorageManager()
+
+    def record_failure(
+
+        self,
+
+        layer,
+
+        batch_id,
+
+        task_name,
+
+        error,
+
+        retry_count
+
+    ):
+
+        try:
+
+            failure_df = spark.createDataFrame(
+
+                [
+
+                    Row(
+
+                        layer=layer,
+
+                        batch_id=batch_id,
+
+                        task_name=task_name,
+
+                        error_message=str(error),
+
+                        retry_count=retry_count,
+
+                        failure_time=
+                            datetime.now()
+
+                    )
+
+                ]
+
+            )
+
+            self.storage.save_delta(
+
+                failure_df,
+
+                FAILED_EXECUTION_PATH,
+
+                mode="append"
+
+            )
+
+            self.logger.error(
+
+                f"Failure Recorded : "
+                f"{task_name}"
+
+            )
+
+        except Exception as e:
+
+            self.logger.error(
+
+                f"Failure Handler Error : "
+                f"{str(e)}"
+
+            )
+
+            raise
+
+    def classify_failure(
+
+        self,
+
+        error_message
+
+    ):
+
+        error_message = (
+            str(error_message).lower()
+        )
+
+        if "access denied" in error_message:
+
+            return "PERMISSION"
+
+        elif "network" in error_message:
+
+            return "NETWORK"
+
+        elif "timeout" in error_message:
+
+            return "TIMEOUT"
+
+        elif "not found" in error_message:
+
+            return "SOURCE_PATH"
+
+        else:
+
+            return "UNKNOWN"
